bd9250dc39efbc798fbbb40be2a31aaddf8eac77
Nitish Itankar <nitishitankar9102@gmail.com>
2024-08-08 00:02:45 +0530

    Newer UI

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import TodoForm from './TodoForm';
+import TodoList from './TodoList';
+import TodoItem from './TodoItem';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem('todos')) || [];
+    setTodos(storedTodos);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const addTodo = (todo) => {
+    setTodos([...todos, { id: Date.now(), text: todo, completed: false }]);
+  };
+
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <div className="app">
+      <h1>Daily Todo List</h1>
+      <TodoForm addTodo={addTodo} />
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+    </div>
+  );
+}
+
+export default App;
